a45e8b0982ff72e2d12f45461da1c9464fe21778
PiotrBerebecki <petebere@gmail.com>
2017-02-02 12:33:21 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## React Note Taking App
+
+### Live version: https://react-note-taking-app.herokuapp.com/
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 Below you will find some information on how to perform common tasks.<br>
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+      
     );
   }
 }
